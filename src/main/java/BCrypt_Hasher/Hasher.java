e7122e8c4dfc3a154f19c773354f0231e14af8c4
unknown <christophertmontoya@gmail.com>
2024-02-26 17:53:38 -0700

    Fixed the body

--- src/main/java/BCrypt_Hasher/Hasher.java
+++ src/main/java/BCrypt_Hasher/Hasher.java
@@ -1,2 +1,15 @@
-package BCrypt_Hasher;public class Hasher {
+package BCrypt_Hasher;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Hasher {
+    private static int log_rounds = 10;
+    public static String hash(String plain) {
+        String salt = BCrypt.gensalt(log_rounds);
+        return BCrypt.hashpw(plain, salt);
+    }
+
+    public static boolean verify_hash(String plain, String hash) {
+        return BCrypt.checkpw(plain, hash);
+    }
 }
