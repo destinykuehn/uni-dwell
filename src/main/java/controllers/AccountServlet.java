50ccd24cf7e19888aef1d4407f569a803a78de17
unknown <christophertmontoya@gmail.com>
2024-02-27 11:25:22 -0700

    Added verbose setting and method to check if email is valid

--- src/main/java/controllers/AccountServlet.java
+++ src/main/java/controllers/AccountServlet.java
@@ -6,8 +6,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,6 +16,8 @@ public class AccountServlet extends HttpServlet {
     /* Exception logger */
     private static final Logger logger = Logger.getLogger(IndexServlet.class.getName());
 
+    private static boolean verbose = true;
+
     public AccountServlet() {
         super();
     }
@@ -27,9 +27,13 @@ public class AccountServlet extends HttpServlet {
         try {
             System.out.printf("Hello from %s%n", this.getClass().getName());
             if (req.getParameter("sign-up-email") != null && req.getParameter("sign-up-password") != null) {
-                handleSignUp(req);
+                if (verbose) System.out.println("Sign up attempted");
+                int res = handleSignUp(req);
+                if (res != 0) System.out.println("Rejected sign up");
             } else if (req.getParameter("sign-in-email") != null && req.getParameter("sign-in-password") != null) {
-                handleSignIn(req);
+                if (verbose) System.out.println("Sign in attempted");
+                int res = handleSignIn(req);
+                if (res != 0) System.out.println("Rejected sign in");
             }
         } catch (Exception e) {
             handleException(e);
@@ -43,38 +47,51 @@ public class AccountServlet extends HttpServlet {
         logger.throwing(ExampleServlet.class.getName(), "methodName", e);
     }
 
-    private void handleSignIn(HttpServletRequest req) {
-
+    private int handleSignIn(HttpServletRequest req) {
+        return 0;
     }
 
     private int handleSignUp(HttpServletRequest req) {
         String sign_up_email = req.getParameter("sign-up-email");
         String sign_up_pass = req.getParameter("sign-up-password");
-//        System.out.printf("%s\n", sign_up_email);
-//        System.out.printf("%s\n", Hasher.hash(sign_up_pass));
+        if (verbose) {
+            System.out.printf("Email: %s\n", sign_up_email);
+            System.out.printf("Pass: %s\n", sign_up_pass);
+        }
         if (!checkPasswordValid(sign_up_pass)) {
+            if (verbose) System.out.println("Invalid password. Rejecting sign up...");
             return 1;
         }
+        if (!checkEmailValid(sign_up_email)) {
+            if (verbose) System.out.println("Invalid email. Rejecting sign up...");
+            return 1;
+        }
+        if (verbose) System.out.println("Valid email and password. Proceeding with sign in...");
         String hash_pw = Hasher.hash(sign_up_pass);
-        if (isUserInDB(sign_up_email, sign_up_pass) == 0) {
-            System.out.println("Yes");
+        if (isUserInDB(sign_up_email) == 0) {
+            System.out.println("User in database");
         } else {
-            System.out.println("No");
+            System.out.println("User not in database");
         }
         return 0;
     }
 
+    private boolean checkEmailValid(String email) {
+        Pattern pattern_email = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher matcher_email = pattern_email.matcher(email);
+        return matcher_email.matches();
+    }
+
     private boolean checkPasswordValid(String password) {
-        return (containsDigit(password) && containsSpecial(password) && containsUpper(password));
+        return (containsDigit(password) && containsSpecial(password)
+                && containsUpper(password) && password.length() <= 20);
     }
 
     private boolean containsDigit(String pw) {
         Pattern pattern_digit = Pattern.compile(".*\\d.*");
         Matcher matcher_digit = pattern_digit.matcher(pw);
-        if (matcher_digit.find()) {
-            System.out.println("The string contains a number.");
-        } else {
-            System.out.println("The string does not contain a number.");
+        if (!matcher_digit.find()) {
+            if (verbose) System.out.println("The string does not contain a number.");
             return false;
         }
         return true;
@@ -83,12 +100,11 @@ public class AccountServlet extends HttpServlet {
     private boolean containsSpecial(String pw) {
         Pattern pattern_special = Pattern.compile("[^a-zA-Z0-9\\s]");
         Matcher matcher_special = pattern_special.matcher(pw);
-        if (matcher_special.find()) {
-            System.out.println("The string contains special char");
-        } else {
-            System.out.println("The string does not contain special char");
+        if (!matcher_special.find()) {
+            if (verbose) System.out.println("The string does not contain special char");
             return false;
         }
+        if (verbose) System.out.println("The string contains a special char");
         return true;
     }
 
@@ -97,15 +113,20 @@ public class AccountServlet extends HttpServlet {
         for (int i = 0; i < pw.length(); i++) {
             char c = pw.charAt(i);
             if (Character.isUpperCase(c)){
-                System.out.println("Contains uppercase");
+                if (verbose) System.out.println("String contains an uppercase");
                 upper = true;
                 break;
             }
         }
+        if (verbose && !upper) System.out.println("String does not contain an uppercase");
         return upper;
     }
 
-    private int isUserInDB(String user, String ps) {
-        return DatabaseServlet.accessDatabase("admin", "Users", "email", user, "find");
+    private int isUserInDB(String email) {
+        return DatabaseServlet.accessDatabase("admin", "Users", "email", email, "find");
+    }
+
+    private int insertUserIntoDB(String email, String pw) {
+        return 0;
     }
 }
